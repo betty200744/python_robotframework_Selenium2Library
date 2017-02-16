721d77ebd851fd05604798e118f10a5c593aa1ab
ylzhao <htzha0200744@gmail.com>
2017-02-16 16:37:07 +0800

    modify the parse_rss, suport get comtent from element tag

--- v2ex/V2ex_Libraries.py
+++ v2ex/V2ex_Libraries.py
@@ -6,6 +6,7 @@ import requests
 import re
 
 
+
 hackernew_url = 'https://news.ycombinator.com/rss'
 
 class HackerNews():
@@ -15,8 +16,8 @@ class HackerNews():
     def get_title(self):
         return self.data.get('title')
 
-    def get_link(self):
-        return self.data.get('link')
+    def get_comments(self):
+        return self.data.get('comments')
 
 
 def get_rss(url):
@@ -29,18 +30,24 @@ def parse_rss(url):
     # link = re.compile('<a href="(.*)">')
     data = {}
     soup = BeautifulSoup(get_rss(url), 'lxml')
-    title = soup.find_all('title')
-    link = soup.find_all('link')
-    data.update(title=title[0], link=link[0])
+    print soup
+    title = soup.select('title')[0].contents[0]
+    comments = soup.select('comments')[0].contents[0]
+    data.update(title=title, comments=comments)
     return data
 
 
 def get_topics_title():
     return HackerNews(parse_rss(hackernew_url)).get_title()
 
-def get_topics_link():
-    return HackerNews(parse_rss(hackernew_url)).get_link()
+def get_topics_comments():
+    return HackerNews(parse_rss(hackernew_url)).get_comments()
 
 
 if __name__ == "__main__":
-    print HackerNews(parse_rss(hackernew_url)).get_title()
+    a = HackerNews(parse_rss(hackernew_url))
+    print a.get_comments()
+    print a.get_title()
+
+
+
